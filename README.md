3e42090b6c2693e7cc07817c041df5e08913ebc2
georgehawkins0 <georgefh99@gmail.com>
2022-05-23 17:51:37 +0100

    V1 i assume

--- README.md
+++ README.md
@@ -1,2 +1,180 @@
 # insta-tz
  
+Get analysis of instagram user post time from a cli based OSINT application.
+
+# Table of contents
+
+- [Installation](#installation)
+- [Start](#start)
+- [Usage](#usage)
+- [About / Further reading](#about)
+
+
+# Installation
+
+
+Clone the github repo
+```
+$ git clone https://github.com/georgehawkins0/insta-tz.git
+```
+Change Directory
+
+```
+$ cd insta-tz
+```
+Install requirements
+```
+$ python3 -m pip install -r requirements.txt
+```
+
+# Start
+To use this tool, you will need an instagram account. If the users you wish to analyse are private, this instagram account needs to be following them. 
+
+You need to add login credentials for the account you wish to do the crawling from in creds/creds.yml
+
+
+# Usage
+
+## Example usage
+
+1. Analyse user @github
+
+        $ tz.py --target github
+
+
+2. Analyse post time of @github, but only crawl first 250 posts
+
+        $ tz.py --target github --limit 250
+
+## Args list
+
+
+```
+usage: tz.py [-h] [-t TARGET] [-l LIMIT] [-p]
+
+Analysis of user post time.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -t TARGET, --target TARGET
+                        Target username
+  -l LIMIT, --limit LIMIT
+                        Post limit to crawl.
+  -p, --percent         Display percentages on the hour frequency graph
+```
+
+
+# Example Output
+```
+        ██╗███╗░░██╗░██████╗████████╗░█████╗░    ████████╗███████╗
+        ██║████╗░██║██╔════╝╚══██╔══╝██╔══██╗    ╚══██╔══╝╚════██║
+        ██║██╔██╗██║╚█████╗░░░░██║░░░███████║    ░░░██║░░░░░███╔═╝
+        ██║██║╚████║░╚═══██╗░░░██║░░░██╔══██║    ░░░██║░░░██╔══╝░░
+        ██║██║░╚███║██████╔╝░░░██║░░░██║░░██║    ░░░██║░░░███████╗
+        ╚═╝╚═╝░░╚══╝╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝    ░░░╚═╝░░░╚══════╝
+                    Instagram user OSINT/SOCMINT
+                      github.com/georgehawkins0
+
+
+Fetching posts: 100%|█████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████| 304/304 [00:20<00:00, 15.10it/s]
+Hour   Frequency
+0      ████████
+1      █████████████
+2      █████
+3      ██████
+4
+5
+6      ███
+7
+8      ██████
+9      █
+10     █████
+11     ███
+12     ████████
+13     ████
+14     ████
+15     ██████
+16     ████████████████
+17     █████████████████████████████
+18     ██████████████████████████████████████████
+19     ████████████████████████████████
+20     ██████████████████████████████████
+21     ████████████████████████████
+22     █████████████████████████
+23     ██████████████████████████
+
+ Estimated timezone: gmt -2
+
+
+ Analysis of @github
+ ```
+
+ # About and Further reading
+
+## Requirements
+
+ The actual data collection makes use of [instagram_private_api](https://github.com/ping/instagram_private_api). [TQDM](https://github.com/tqdm/tqdm) is used for the progress bar and [Rich](https://github.com/Textualize/rich) is used for the pretty red colour of the banner.
+
+## Further
+This application generates a frequency per hour of post upload time of the target. Using this, it tries to work out the timezone of a user by taking into account the time the user will be inactive while asleep. However everone has different lifestyles and as such this command is probably only accurate +/-3 hours, maybe more. However it is still a very useful insight into a users lifestyle.
+
+Using this graph, you can also very quickly work out if the posts on the account are botted (if not obvious already) or posted by multiple people. A botted account will have no obvious rest period perhaps as such:
+```Hour   Frequency
+0      ████████
+1      ██████████
+2      █████████
+3      █████████
+4      ██████████
+5      ██████████
+6      █████████
+7      ████████
+8      █████████
+9      ███████████
+10     █████████
+11     █████████
+12     ██████████
+13     █████████
+14     ██████████
+15     █████████
+16     ████████
+17     ██████████
+18     █████████
+19     ███████
+20     ████████
+21     ████████
+22     █████████
+23     ███████
+```
+
+However, an account which is not botted will look much more like this:
+
+```
+Hour   Frequency
+0      ████████
+1      █████████████
+2      █████
+3      ██████
+4
+5
+6      █
+7
+8      ████
+9      █
+10     █████
+11     ███
+12     ████████
+13     ████
+14     ████
+15     ██████
+16     ████████████████
+17     █████████████████████████████
+18     ██████████████████████████████████████████
+19     ████████████████████████████████
+20     ██████████████████████████████████
+21     ████████████████████████████
+22     █████████████████████████
+23     ██████████████████████████
+```
+Even glancing at this graph you can see obvious fluxuations in activity over time of day.
+
+I have found this tool to be very informative if there is nothing other to analyse of a user. 
---
+++ tz.py
@@ -0,0 +1,263 @@
+from instagram_private_api import Client, ClientError, ClientLoginError, ClientCookieExpiredError, ClientLoginRequiredError
+import yaml
+from yaml.loader import FullLoader, Loader
+import codecs
+import json
+import argparse
+from tqdm import tqdm
+import sys
+import os
+from datetime import datetime
+from rich.console import Console
+
+def get_password():
+        try:
+            password = yaml.load(open('./creds/creds.yml'), Loader=FullLoader)
+            passwd = password['password']
+            if passwd:
+                return str(passwd)
+            else:
+                print("Error in creds.yml. Are your credentials in the correct format? \nusername: <username> \npassword: <password>")
+                sys.exit()
+        except FileNotFoundError:
+            print("Error: file not found")
+            print("\n")
+        except TypeError as e:
+            if str(e) == "string indices must be integers":
+                print("Error in creds.yml. Are your credentials in the correct format? \nusername: <username> \npassword: <password>")
+                sys.exit()
+
+def get_username():
+    try:
+        username = yaml.load(open('./creds/creds.yml'), Loader=FullLoader)
+        user = username['username']
+        if user:
+            return str(user)
+        else:
+            print("Error in creds.yml. Are your credentials in the correct format? \nusername: <username> \npassword: <password>")
+            sys.exit()
+    except FileNotFoundError:
+        print("Error: file not found")
+        print("\n")
+    except TypeError as e:
+        if str(e) == "string indices must be integers":
+                print("Error in creds.yml. Are your credentials in the correct format? \nusername: <username> \npassword: <password>")
+                sys.exit()
+
+def to_json(python_object):
+    if isinstance(python_object, bytes):
+        return {'__class__': 'bytes',
+                '__value__': codecs.encode(python_object, 'base64').decode()}
+    raise TypeError(repr(python_object) + ' is not JSON serializable')
+
+def from_json(json_object):
+    if '__class__' in json_object and json_object['__class__'] == 'bytes':
+        return codecs.decode(json_object['__value__'].encode(), 'base64')
+    return json_object
+
+def onlogin_callback(api, new_settings_file):
+    cache_settings = api.settings
+    with open(new_settings_file, 'w') as outfile:
+        json.dump(cache_settings, outfile, default=to_json)
+        print('SAVED: {0!s}'.format(new_settings_file))
+
+def login():
+    username = get_username()
+    password = get_password()
+    
+
+    settings_file_path = "./creds/settings.json"
+    device_id = None
+    try:
+            settings_file = settings_file_path
+            if not os.path.isfile(settings_file):
+                # settings file does not exist
+                print('Unable to find file: {0!s}'.format(settings_file))
+
+                # login new
+                api = Client(
+                    username, password,
+                    on_login=lambda x: onlogin_callback(x, settings_file_path))
+            else:
+                with open(settings_file) as file_data:
+                    cached_settings = json.load(file_data, object_hook=from_json)
+                print('Reusing settings: {0!s}'.format(settings_file))
+
+                device_id = cached_settings.get('device_id')
+                # reuse auth settings
+                api = Client(
+                    username, password,
+                    settings=cached_settings)
+
+    except (ClientCookieExpiredError, ClientLoginRequiredError) as e:
+        print('ClientCookieExpiredError/ClientLoginRequiredError: {0!s}'.format(e))
+
+        # Login expired
+        # Do relogin but use default ua, keys and such
+        api = Client(
+            username, password,
+            device_id=device_id,
+            on_login=lambda x: onlogin_callback(x, settings_file_path))
+
+    except ClientError as err:
+            e = json.loads(err.error_response)
+            print(e['message'])
+            print(err.msg)
+            print("\n")
+            if 'challenage' in e:
+                print("Please follow link to complete the challange " + e['challange']['url'])
+            sys.exit()
+
+    return api
+
+def check_following(target_id):
+    if str(target_id) == api.authenticated_user_id:
+        return True
+    endpoint = 'users/{user_id!s}/full_detail_info/'.format(**{'user_id' : target_id})
+    return api._call_api(endpoint)['user_detail']['user']['friendship_status']['following']
+
+def get_posts(target_id):
+    data = []
+    res = api.user_feed(str(target_id))
+    data.extend(res.get("items", []))
+
+    next_max_id = res.get('next_max_id')
+    while next_max_id:
+        results = api.user_feed(
+            str(target_id), max_id=next_max_id)
+        data.extend(results.get('items', []))
+        if args.limit: # if there is a limit
+            if len(data) >= args.limit: # if the current post list is bigger or equal to limit then return it 
+                return data
+        tq.update(len(results.get('items', [])))
+        next_max_id = results.get('next_max_id')
+
+    return data
+
+def can_crawl(target_id=None,username=None):
+    if not target_id:
+        content =  api.username_info(username)
+        target_id = content['user']['pk']
+        is_private = content['user']['is_private']
+        following = check_following(target_id)
+    else:
+        content =  api.user_info(target_id)
+        target_id = content['user']['pk']
+        is_private = content['user']['is_private']
+        following = check_following(target_id)
+    if is_private and not following:
+        return False
+    return True
+
+def get_id(username):
+    content =  api.username_info(username)
+    id = content['user']['pk']
+    return id
+
+def get_number_of_posts(username=None,target_id=None):
+    if not target_id:
+        content =  api.username_info(username)
+        target_id = content['user']['pk']
+    content = api.user_info(str(target_id))
+    return int(content["user"]["media_count"])
+
+def timezonen(hoursDict):
+    GMTBEDTIME=25
+    GMTBEDTIME_T = 1
+    lowest=float("infinity")
+    for hour in hoursDict:
+        hour = int(hour)
+        total = 0
+        total+=hoursDict[hour]
+        total+=hoursDict[(hour+1)%24]
+        total+=hoursDict[(hour+2)%24]
+        total+=hoursDict[(hour+3)%24]
+        total+=hoursDict[(hour+4)%24] # mod because it will go to hour 25, 26 ect which is 1 and 2am
+        if lowest>total:
+            lowest = total
+            bedtime = hour
+
+    if bedtime >11:
+        timezone = GMTBEDTIME-bedtime
+    else:
+        timezone = GMTBEDTIME_T-bedtime
+    if timezone>-1:
+        sign="+"
+    else:
+        sign=""
+    return("Estimated timezone: gmt {}{}".format(sign,timezone))
+
+def clear():
+    # for windows screen
+    if sys.platform.startswith('win'):
+        os.system('cls')
+    # for mac or linux          
+    else:
+        os.system('clear')
+
+def banner():
+    clear()
+    pc.print("""
+        ██╗███╗░░██╗░██████╗████████╗░█████╗░    ████████╗███████╗
+        ██║████╗░██║██╔════╝╚══██╔══╝██╔══██╗    ╚══██╔══╝╚════██║
+        ██║██╔██╗██║╚█████╗░░░░██║░░░███████║    ░░░██║░░░░░███╔═╝
+        ██║██║╚████║░╚═══██╗░░░██║░░░██╔══██║    ░░░██║░░░██╔══╝░░
+        ██║██║░╚███║██████╔╝░░░██║░░░██║░░██║    ░░░██║░░░███████╗
+        ╚═╝╚═╝░░╚══╝╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝    ░░░╚═╝░░░╚══════╝
+                    Instagram user OSINT/SOCMINT
+                      github.com/georgehawkins0\n\n""",style="bold red")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Analysis of user post time.")
+    parser.add_argument('-t', '--target', type=str, default=None, help="Target username")
+    parser.add_argument('-l', '--limit', type=int, default=None, help="Post limit to crawl.")
+    parser.add_argument('-p', '--percent', action='store_true',help='Display percentages on the hour frequency graph')
+    args = parser.parse_args()
+
+    api = login()
+    pc = Console()
+
+    if args.target:
+        banner()
+        if can_crawl(username=args.target):
+            times = []
+            target_id = get_id(args.target)
+            if args.limit:
+                total = args.limit
+            else:
+                total = get_number_of_posts(target_id=target_id)
+            tq = tqdm(total=total,desc="Fetching posts")
+            # getting posts:
+            posts = get_posts(target_id)
+            d = {n: 0 for n in range(24)}
+            for post in posts:
+                timestamp = post["taken_at"]
+                dt_object = datetime.fromtimestamp(timestamp)
+                d[dt_object.hour] +=1
+
+            tq.update(total-tq.n)
+            tq.close()
+
+            # visual
+            total = len(posts)
+            if args.percent:
+                print("Hour      %   Frequency\n")
+            else:
+                print("Hour   Frequency")
+            for row in d:
+                percent = round((d[row]/total)*100)
+                if args.percent:
+                    percent_space = (9-len(str(row)))*" "
+                    block_space = (5-len(str(percent)))*" "
+                    print("{}{}{}{}".format(row,percent_space,f"{percent}%",block_space),end="")
+                else:
+                    print("{}{}".format(row,(7-len(str(row)))*" "),end="")
+
+                print("█"*d[row])
+            
+            tz = timezonen(d)
+            print("\n",tz)
+            print(f"\n\n Analysis of @{args.target}")
+            
+        else:
+            print("Cannot crawl user. Perhaps you are not following them? ")
